eaadc8cb674f7c68bbd11447ec300e90567f657e
ryuuzera <faag_@live.com>
2025-04-09 22:15:22 -0300

    general fixes

--- Workshop.Service.Bus.Interface/Program.cs
+++ Workshop.Service.Bus.Interface/Program.cs
@@ -1,22 +1,17 @@
-using Swashbuckle.AspNetCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Adiciona appsettings.json e variáveis de ambiente (opcional, mas explícito)
 builder.Configuration
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
     .AddEnvironmentVariables();
 
-// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(); // Se estiver usando o pacote Swashbuckle
+builder.Services.AddSwaggerGen(); 
 
-var app = builder.Build();
+var app = builder.Build(); 
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
